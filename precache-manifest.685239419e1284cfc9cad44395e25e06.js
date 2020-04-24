cbab9bb9cfb8e2e7f443327740ec1f8a64b39b5e
Tommaso <tommaso.vsr@gmail.com>
2020-04-24 19:18:24 +0200

    Updates

---
+++ precache-manifest.685239419e1284cfc9cad44395e25e06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adc35f3c7197d19ac66c7ac6bfcd006",
+    "url": "/gggnz/index.html"
+  },
+  {
+    "revision": "70eb95f25f85db7ed0cb",
+    "url": "/gggnz/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "6be5750ebc461c6e55b7",
+    "url": "/gggnz/static/css/main.36a1c5c2.chunk.css"
+  },
+  {
+    "revision": "70eb95f25f85db7ed0cb",
+    "url": "/gggnz/static/js/2.96cf8688.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gggnz/static/js/2.96cf8688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be5750ebc461c6e55b7",
+    "url": "/gggnz/static/js/main.b8897940.chunk.js"
+  },
+  {
+    "revision": "2bd992bf4a1dbfb3443b",
+    "url": "/gggnz/static/js/runtime-main.8b86a174.js"
+  },
+  {
+    "revision": "03a1efe38019e2a386b07f2466efc4a4",
+    "url": "/gggnz/static/media/facade.03a1efe3.jpg"
+  }
+]);
